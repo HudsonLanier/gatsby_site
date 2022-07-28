a091f4c8f46cc40b9ad65b06c6a2d15d13fbcca8
Hudson Lanier <Hudsonlanier@gmail.com>
2022-07-28 10:57:42 -0400

    creating my react app on Heroku

---
+++ src/pages/testpage.js
@@ -0,0 +1,22 @@
+import * as React from "react"
+import TemplateWrapper from "../components/Layout"
+
+export default function Component () {
+  return (
+  <TemplateWrapper>
+
+<title>Home</title>
+<link rel="stylesheet" type="text/css" href="/src/pages/test.sass"></link>
+
+<body>
+<div class="snow_wrap">
+	<div class="snow"></div>
+</div>
+
+
+</body>
+
+
+
+  </TemplateWrapper>)
+}
